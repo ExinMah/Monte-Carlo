4ff589c862a3fd3ef138d5c690a2093c1405e517
ExinMah <mahe-wp20@student.tarc.edu.my>
2023-08-28 15:15:48 +0800

    Add functions and timer

--- Monte Carlo/Monte Carlo/main.cpp
+++ Monte Carlo/Monte Carlo/main.cpp
@@ -1,27 +1,21 @@
 #include <iostream>
 #include <random>
 #include <vector>
+#include <chrono>
 using namespace std;
 
-int main() {
-    double initialInvestment = 10000.0; // Initial investment amount
-    double expectedReturn = 0.08; // Expected annual return
-    double volatility = 0.15; // Annual volatility (standard deviation)
-    int numSimulations = 10000; // Number of simulations
-    int investmentPeriod = 5; // Investment period in years
-
-    // Create a random number generator
+// Function to perform Monte Carlo simulations
+vector<double> performSimulations(double initialInvestment, double expectedReturn,
+    double volatility, int numSimulations, int investmentPeriod) {
     random_device rd;
     mt19937 gen(rd());
     normal_distribution<double> dis(0.0, 1.0);
 
     vector<double> finalReturns;
 
-    // Perform the Monte Carlo simulations
     for (int i = 0; i < numSimulations; ++i) {
         double cumulativeReturn = 1.0;
 
-        // Simulate investment returns for each year
         for (int j = 0; j < investmentPeriod; ++j) {
             double randomValue = dis(gen);
             double investmentReturn = expectedReturn + volatility * randomValue;
@@ -32,11 +26,15 @@ int main() {
         finalReturns.push_back(finalReturn);
     }
 
-    // Calculate various risk metrics
-    double averageReturn = 0.0;
+    return finalReturns;
+}
+
+// Function to calculate risk metrics
+void calculateRiskMetrics(const vector<double>& finalReturns, double& averageReturn,
+    double& standardDeviation, double& minReturn, double& maxReturn) {
     double totalReturns = 0.0;
-    double minReturn = finalReturns[0];
-    double maxReturn = finalReturns[0];
+    minReturn = finalReturns[0];
+    maxReturn = finalReturns[0];
 
     for (const auto& returnVal : finalReturns) {
         totalReturns += returnVal;
@@ -48,18 +46,21 @@ int main() {
         }
     }
 
-    averageReturn = totalReturns / numSimulations;
-    double variance = 0.0;
-    double standardDeviation = 0.0;
+    averageReturn = totalReturns / finalReturns.size();
 
+    double variance = 0.0;
     for (const auto& returnVal : finalReturns) {
         variance += (returnVal - averageReturn) * (returnVal - averageReturn);
     }
 
-    variance /= (numSimulations - 1);
+    variance /= (finalReturns.size() - 1);
     standardDeviation = sqrt(variance);
+}
 
-    // Display the results
+// Function to display the results
+void displayResults(double initialInvestment, double expectedReturn, double volatility,
+    int investmentPeriod, int numSimulations, double averageReturn,
+    double standardDeviation, double minReturn, double maxReturn, double elapsedTime) {
     cout << "Risk Assessment Results:" << endl;
     cout << "------------------------" << endl;
     cout << "Initial Investment: $" << initialInvestment << endl;
@@ -72,6 +73,31 @@ int main() {
     cout << "Standard Deviation of Returns: " << (standardDeviation * 100.0) << "%" << endl;
     cout << "Minimum Return: " << (minReturn * 100.0) << "%" << endl;
     cout << "Maximum Return: " << (maxReturn * 100.0) << "%" << endl;
+    cout << "Elapsed Time: " << elapsedTime << " seconds" << endl;
+}
+
+int main() {
+    double initialInvestment = 10000.0;
+    double expectedReturn = 0.08;
+    double volatility = 0.15;
+    int numSimulations = 10000;
+    int investmentPeriod = 5;
+
+    // Start the timer
+    auto startTime = chrono::steady_clock::now();
+
+    vector<double> finalReturns = performSimulations(initialInvestment, expectedReturn,
+        volatility, numSimulations, investmentPeriod);
+
+    double averageReturn, standardDeviation, minReturn, maxReturn;
+    calculateRiskMetrics(finalReturns, averageReturn, standardDeviation, minReturn, maxReturn);
+
+    // Stop the timer
+    auto endTime = chrono::steady_clock::now();
+    double elapsedTime = chrono::duration<double>(endTime - startTime).count();
+
+    displayResults(initialInvestment, expectedReturn, volatility, investmentPeriod, numSimulations,
+        averageReturn, standardDeviation, minReturn, maxReturn, elapsedTime);
 
     return 0;
 }
