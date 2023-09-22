c1b340897c6adf78c2c779b8b7ad7a509cfb8e52
ExinMah <mahe-wp20@student.tarc.edu.my>
2023-09-22 14:55:29 +0800

    Add Monte Carlo Ingration

--- Monte Carlo/Monte Carlo/main.cpp
+++ Monte Carlo/Monte Carlo/main.cpp
@@ -1,10 +1,37 @@
 #include <iostream>
 #include <random>
 #include <vector>
+#include <cmath>
 #include <chrono>
-using namespace std;
 
-// Function to perform Monte Carlo simulations
+using namespace std;
+using namespace chrono;
+
+// Function to integrate: f(x) = (1 + r)^(-t)
+double functionToIntegrate(double x) {
+    double r = 0.08; // Annual discount rate 
+    double t = x; // Time in years 
+
+    return pow(1.0 + r, -t);
+}
+
+double monteCarloIntegration(double (*function)(double), double lowerBound, double upperBound, int numSamples) {
+    random_device rd;
+    mt19937 generator(rd());
+    uniform_real_distribution<double> distribution(lowerBound, upperBound);
+
+    double sum = 0.0;
+    for (int i = 0; i < numSamples; ++i) {
+        double x = distribution(generator);
+        sum += function(x);
+    }
+
+    double intervalLength = upperBound - lowerBound;
+    double average = sum / numSamples;
+    return average * intervalLength;
+}
+
+// Perform Monte Carlo Simulations
 vector<double> performSimulations(double initialInvestment, double expectedReturn, double volatility, int numSimulations, int investmentPeriod) {
     random_device rd;
     mt19937 gen(rd());
@@ -55,8 +82,7 @@ void calculateRiskMetrics(const vector<double>& finalReturns, double& averageRet
     standardDeviation = sqrt(variance);
 }
 
-// Function to display the results
-void displayResults(double initialInvestment, double expectedReturn, double volatility, int investmentPeriod, int numSimulations, double averageReturn, double standardDeviation, double minReturn, double maxReturn, double elapsedTime) {
+void displayResults(double initialInvestment, double expectedReturn, double volatility, int investmentPeriod, int numSimulations, double averageReturn, double standardDeviation, double minReturn, double maxReturn, double integrationResult, double elapsedTime) {
     cout << "Risk Assessment Results:" << endl;
     cout << "------------------------" << endl;
     cout << "Initial Investment: $" << initialInvestment << endl;
@@ -69,6 +95,7 @@ void displayResults(double initialInvestment, double expectedReturn, double vola
     cout << "Standard Deviation of Returns: " << (standardDeviation * 100.0) << "%" << endl;
     cout << "Minimum Return: " << (minReturn * 100.0) << "%" << endl;
     cout << "Maximum Return: " << (maxReturn * 100.0) << "%" << endl;
+    cout << "Monte Carlo Integration Result: " << integrationResult << endl;
     cout << "Elapsed Time: " << elapsedTime << " seconds" << endl;
 }
 
@@ -78,20 +105,22 @@ int main() {
     double volatility = 0.15;
     int numSimulations = 1000000;
     int investmentPeriod = 5;
+    double lowerBound = 0.0;
+    double upperBound = 1.0;
+    int numSamples = 1000000;
 
-    // Start the timer
-    auto startTime = chrono::steady_clock::now();
+    auto startTime = steady_clock::now();
 
     vector<double> finalReturns = performSimulations(initialInvestment, expectedReturn, volatility, numSimulations, investmentPeriod);
-
     double averageReturn, standardDeviation, minReturn, maxReturn;
     calculateRiskMetrics(finalReturns, averageReturn, standardDeviation, minReturn, maxReturn);
 
-    // Stop the timer
-    auto endTime = chrono::steady_clock::now();
-    double elapsedTime = chrono::duration<double>(endTime - startTime).count();
+    double integrationResult = monteCarloIntegration(functionToIntegrate, lowerBound, upperBound, numSamples);
 
-    displayResults(initialInvestment, expectedReturn, volatility, investmentPeriod, numSimulations, averageReturn, standardDeviation, minReturn, maxReturn, elapsedTime);
+    auto endTime = steady_clock::now();
+    double elapsedTime = duration<double>(endTime - startTime).count();
+
+    displayResults(initialInvestment, expectedReturn, volatility, investmentPeriod, numSimulations, averageReturn, standardDeviation, minReturn, maxReturn, integrationResult, elapsedTime);
 
     return 0;
 }
